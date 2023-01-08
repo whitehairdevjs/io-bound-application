addcf54d63a70058f15df5e3a81ef2f1fad22b4a
whitehairdevjs <jsuserwork20@gmail.com>
2023-01-08 22:01:08 +0900

    feat: rabbitmq 추가

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -1,5 +1,7 @@
 package class101.foo.io;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,10 +18,18 @@ public class PostController {
     @Autowired
     PostRepository postRepository;
 
+    @Autowired
+    Producer producer;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
     // 1. 글을 작성한다.
     @PostMapping("/post")
-    public Post createPost(@RequestBody Post post) {
-        return postRepository.save(post);
+    public Post createPost(@RequestBody Post post) throws JsonProcessingException {
+        String jsonPost = objectMapper.writeValueAsString(post);
+        producer.sendTo(jsonPost);
+        return post;
     }
 
     // 2 글 목록을 페이징하여 반환
@@ -36,7 +46,7 @@ public class PostController {
         return postRepository.findById(id).get();
     }
 
-    // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
+    // 4.
     @GetMapping("/search")
     public List<Post> findPostsByContent(@RequestParam String content) {
         return postRepository.findByContentContains(content);
